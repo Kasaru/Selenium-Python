ff4fc45a8e66f9054b9107e7d70946ceca987395
Kasaru <58173553+Kasaru@users.noreply.github.com>
2023-01-23 02:14:03 +0400

    First_chapter selenium

--- Finding_elements_with_Selenium/lesson6_step4.py
+++ Finding_elements_with_Selenium/lesson6_step4.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-link = "http://suninjuly.github.io/simple_form_find_task.html"
+link = "http://suninjuly.github.io/find_link_text"
 
 try:
     browser = webdriver.Chrome()
--- Finding_elements_with_Selenium/lesson6_step5.py
+++ Finding_elements_with_Selenium/lesson6_step5.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
+import math
 from selenium.webdriver.common.by import By
 import time
 
-link = "http://suninjuly.github.io/simple_form_find_task.html"
-
+link = "http://suninjuly.github.io/find_link_text"
+text = str(math.ceil(math.pow(math.pi, math.e)*10000))
 try:
     browser = webdriver.Chrome()
     browser.get(link)
-
+    link = browser.find_element(By.LINK_TEXT, text)
+    link.click()
     input1 = browser.find_element(By.TAG_NAME, "input")
     input1.send_keys("Ivan")
     input2 = browser.find_element(By.NAME, "last_name")
@@ -18,7 +20,6 @@ try:
     input4.send_keys("Russia")
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
-
 finally:
     # успеваем скопировать код за 30 секунд
     time.sleep(30)
